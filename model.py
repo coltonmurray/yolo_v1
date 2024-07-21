a7706540ecd958d160d7737ed65b9d9624e60f08
colton murray <colton.murray123@gmail.com>
2024-07-21 15:33:15 -0400

    fix

--- model.py
+++ model.py
@@ -8,5 +8,19 @@ class cnn_block(nn.module):
         self.batch_norm = nn.BatchNorm2d(out_channels)
         self.leaky_relu = nn.LeakyReLU(0.1)
 
+    def forward(self, x):
+        return self.leaky_relu(self.batch_norm(self.conv(x)))
+    
+
+class yolo(nn.module):
+    def __init__(self, in_channels=3, **kwargs):
+        super(yolo,self).__init__()
+        self.in_channels = in_channels
+        self.darknet = self.create_layers()
+
+
+
+
+
 
         
